1c5dd04f9168e0ab009968497bac14d45852c8f7
Peter Hyman <pete@peterhyman.com>
2022-02-03 09:01:42 -0600

    More error checking. Make options. Kernel release detection.

--- kinstall.sh
+++ kinstall.sh
@@ -9,10 +9,24 @@ error() {
 }
 
 # do some checks
-[ $UID -ne 0 ] && error "Must run as root to install a kernel"
-[ ! -f .config ] && error "No .config file found"
-[ ! -f Makefile ] && error "No Makefile file found"
-[ ! $(which depmod) ] && error "depmod program not found. Using sudo?"
+[ $UID -ne 0 ] && error "Must run as root to install a kernel."
+[ ! -x $(which make) ] && error "SERIOUS ERROR: No make command found!"
+[ ! -f .config ] && error "No .config file found. Run make oldconfig."
+[ ! -f Makefile ] && error "SERIOUS ERROR: No Makefile file found."
+[ ! -x $(which mkinitrd) ] && error "mkinitrd not found. This may be serious."
+[ ! -x $(which depmod) ] && error "depmod program not found. Using sudo?"
+
+# Check for nice
+NICE=$(which nice)
+if [ -x $NICE ]; then
+	# update niceness if desired
+	NICENESS="-n 19"
+else
+	unset NICE
+	unset NICENESS
+fi
+# set make options if desired
+MAKEOPTS="-j8"
 
 # Set some directories and detect any links
 USRDIR="/usr"
@@ -30,33 +44,41 @@ REALPWDIR=`realpath .`
 ### make block ###
 # comment out or alter as desired
 # set up .config file with defaults
+# or change to `make oldconfig` and input new options manually.`
+echo "making oldconfig"
 make olddefconfig
+[ $? -ne 0 ] && error "Error running make olddefconfig."
 # make with -j8 option
-make -j8
+# can split up with
+# make vmlinux
+# make modules
+# instead of make all which is default
+echo "making kernel and modules: $NICE $NICENESS make $MAKEOPTS"
+$NICE $NICENESS make $MAKEOPTS
+[ $? -ne 0 ] && error "Error making kernel."
 # install modules
-make modules_install
+echo "installing modules"
+$NICE $NICENESS make modules_install
+[ $? -ne 0 ] && error "Error installing modules."
 # install headers to /usr
-make headers_install INSTALL_HDR_PATH="$USRDIR"
+echo "installing headers"
+$NICE $NICENESS make headers_install INSTALL_HDR_PATH="$USRDIR"
+[ $? -ne 0 ] && error "Error installing headers."
 
-# use kernel.release to get version if it exists
-# KR = Kernel Release
 # PL = Patch Level
 # SL = Patch Sub Level
 # FV = Final Version
-if [ -r include/config/kernel.release ] ; then
-	KR=`cat include/config/kernel.release`
-	KV=`echo $KR | cut -d . -f 1`
-	PL=`echo $KR | cut -d . -f 2`
-	SL=`echo $KR | cut -d . -f 3`
-else
-# Kernel, Patch, Sublevel, and full kernel version
-# set from Makefile
-	KV=`grep -m 1 "VERSION"		Makefile | cut -d ' ' -f 3`
-	PL=`grep -m 1 "PATCHLEVEL"	Makefile | cut -d ' ' -f 3`
-	SL=`grep -m 1 "SUBLEVEL"	Makefile | cut -d ' ' -f 3`
-fi
+# use make to get either kernelversion or kernelrelease
+# it's possible make throws an error before printing the version, so
+# get the last line only.
+# FV=$(make kernelrelease | tail -n1)
+FV=$(make kernelversion | tail -n1)
+[ $? -ne 0 ] &&  error "Error fetching kernel version."
 
-FV=$KV.$PL.$SL
+echo "Kernel Version is $FV"
+KV=`echo $FV | cut -d . -f 1`
+PL=`echo $FV | cut -d . -f 2`
+SL=`echo $FV | cut -d . -f 3`
 
 FVDIR="$USRSRCDIR/linux-$FV"
 
@@ -75,7 +97,7 @@ ln -vnfs "$FVDIR" /usr/src/linux
 # be sure mkinitrd.conf is set
 echo "Performing mkinitrd"
 
-/sbin/mkinitrd -F /etc/mkinitrd.conf -k $FV -o /boot/initrd-$FV.gz
+$NICE $NICENESS mkinitrd -F /etc/mkinitrd.conf -k $FV -o /boot/initrd-$FV.gz
 
 ### Install initrd and vmlinus ###
 echo "Copying core files to /boot"
@@ -90,4 +112,3 @@ cp -v "$BOOTDIR"/initrd-$FV.gz "$EFIDIR"/initrd.gz
 cp -v "$BOOTDIR"/vmlinuz-$FV "$EFIDIR"/vmlinuz
 
 echo "Be sure to adjust and run lilo, or edit grub.cfg"
-
